67c2bc7d682f83726418253feb201809aa2ea752
it21038914 <it21038914@my.sliit.lk>
2023-05-01 22:53:20 +0530

    5

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,3 +1,14 @@
-import { createContext } from "react";
+import { createContext ,useReducer} from "react";
 
 export const WorkoutContext = createContext();
+
+export const WorkoutContextProvider = ({ children }) => {\
+    const [state,dispatch]= useReducer()
+  return (
+    <WorkoutContext.Provider value={{ workout: [] }}>
+      {children}
+    </WorkoutContext.Provider>
+  );
+};
+
+//oke children la venne WorkoutContextProvider rap vela thiyana athule ewa
